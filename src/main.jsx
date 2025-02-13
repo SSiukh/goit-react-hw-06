b8265cbd313d6d5728980adba20d26cd7adfe444
SSiukh <siukhsasha@gmail.com>
2025-02-13 19:55:18 +0200

    fix persist problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "modern-normalize";
 import "./index.css";
 import App from "./components/App";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate>
       <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
